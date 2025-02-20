9652edbd2e1d4c8b594dc0a1a8a1299c8fec5e63
Ioan-Knowles-2005 <s2494818@ed.ac.uk>
2025-02-20 17:59:48 +0000

    Fixed zerodivision error

--- BSM_Individual_Pricing.py
+++ BSM_Individual_Pricing.py
@@ -18,8 +18,8 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.header("Input Parameters")
-    S = st.number_input("Current Stock Price: ", min_value=float(0), step=0.01, key="S_input")
-    K = st.number_input("Strike Price: ", min_value=float(0), step=0.01, key="K_input")
+    S = st.number_input("Current Stock Price: ", min_value=float(0), step=0.01,value=100, key="S_input")
+    K = st.number_input("Strike Price: ", min_value=float(0), step=0.01, value=100, key="K_input")
     r = st.number_input("Risk-Free Rate(%): ", min_value=float(0), max_value=float(100), value=1.5, step=0.01, key="r_input")
     t = st.number_input("Time Until Maturity (years): ", min_value=float(0), value=1.0, step=0.01, key="t_input")
     sigma = st.number_input("Volatility: ", min_value=float(0), value=0.2,  step=0.01, key="V_input")
