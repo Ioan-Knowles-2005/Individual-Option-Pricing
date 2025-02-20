93f24705122b36bdfac488d4ba6fb5051eba852e
Ioan-Knowles-2005 <s2494818@ed.ac.uk>
2025-02-20 18:41:05 +0000

    Added heatmap

--- BSM_Individual_Pricing.py
+++ BSM_Individual_Pricing.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy as np
 from scipy.stats import norm
-import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def bsm_model(S, K, r, t, sigma):
     if t == 0:
@@ -32,3 +33,22 @@ with col2:
     st.write("### Option Prices")
     st.write("Call Price: ", round(call_price, 2))
     st.write("Put Price: ", round(put_price, 2))
+
+    st.write("### Heatmap: Call Option Price Sensitivity")
+    S_values = np.linspace(S * 0.5, S * 1.5, 50)
+    sigma_values = np.linspace(sigma * 0.5, sigma * 1.5, 50)
+    S_grid, sigma_grid = np.meshgrid(S_values, sigma_values)
+    call_grid = np.zeros_like(S_grid)
+
+    for i in range(S_grid.shape[0]):
+        for j in range(S_grid.shape[1]):
+            call_grid[i, j], _ = bsm_model(S_grid[i, j], K, r_decimal, t, sigma_grid[i, j])
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+    heatmap = ax.pcolormesh(S_grid, sigma_grid, call_grid, shading='auto', cmap='viridis')
+    ax.set_xlabel('Underlying Price (S)')
+    ax.set_ylabel('Volatility (sigma)')
+    ax.set_title('Call Option Price Heatmap')
+    fig.colorbar(heatmap, ax=ax)
+
+    st.pylot(fig)
